3983ab4fb460ffc21710499722dfbaea5db2d97b
Adjy Sedar Desir <desiradjysedar@gmail.com>
2024-02-22 15:25:46 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 with import <nixpkgs> {};
 
 let
-  # masuperlib-src = fetchTarball "TODO";
-  masuperlib-src = "https://github.com/adjy/masuperlib/archive/refs/tags/v0.1.1.0.tar.gz";
+   masuperlib-src = fetchTarball "https://github.com/adjy/masuperlib/archive/refs/tags/v0.1.1.0.tar.gz";
+# masuperlib-src = ../masuperlib;
 
   masuperlib = callPackage masuperlib-src {};
 
